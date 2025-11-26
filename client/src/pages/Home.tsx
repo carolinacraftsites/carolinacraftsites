56da5b5ddf100777131798ede14b87352dfcaa58
NathanKoerschner <nathankoerschner@gmail.com>
2025-11-26 10:34:36 -0500

    Update form

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <Services />
         <Portfolio />
         <Pricing />
-        <ContactForm />
+        {/* <ContactForm /> */}
       </main>
       <Footer />
     </div>
